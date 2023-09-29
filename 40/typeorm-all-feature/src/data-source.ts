dd2147cc93c5b3d0f88f2d56bdd0ec76677435db
thinkasany <480968828@qq.com>
2023-09-29 14:33:20 +0800

    feat(40): typeorm crud test

--- 40/typeorm-all-feature/src/data-source.ts
+++ 40/typeorm-all-feature/src/data-source.ts
@@ -1,17 +1,24 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
+import { Aaa } from "./entity/Aaa"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
+    username: "root",
+    password: "123456",
+    database: "practice",
     synchronize: true,
-    logging: false,
-    entities: [User],
+    logging: true,
+    entities: [User, Aaa],
     migrations: [],
     subscribers: [],
+    poolSize: 10,
+    connectorPackage: 'mysql2',
+    extra: {
+        authPlugin: 'sha256_password',
+    }
 })
+
--- 40/typeorm-all-feature/src/index.ts
+++ 40/typeorm-all-feature/src/index.ts
@@ -1,20 +1,114 @@
 import { AppDataSource } from "./data-source"
 import { User } from "./entity/User"
+import { In } from "typeorm";
 
 AppDataSource.initialize().then(async () => {
+    // const user = new User()
+    // user.id = 1;
+    // user.firstName = "aaa111"
+    // user.lastName = "bbb111"
+    // user.age = 25
+    // await AppDataSource.manager.save(user);
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+    // await AppDataSource.manager.save(User, [
+    //     { firstName: 'ccc', lastName: 'ccc', age: 21},
+    //     { firstName: 'ddd', lastName: 'ddd', age: 22},
+    //     { firstName: 'eee', lastName: 'eee', age: 23}
+    // ]);
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
+    // await AppDataSource.manager.delete(User, 1);
+    // await AppDataSource.manager.delete(User, [2,3]);
 
-    console.log("Here you can setup and run express / fastify / any other framework.")
+    // const user = new User();
+    // user.id = 59;
+    // await AppDataSource.manager.remove(User, user);
+  
+    // await AppDataSource.manager.save(User, [
+    //     { firstName: 'ccc', lastName: 'ccc', age: 21},
+    //     { firstName: 'ddd', lastName: 'ddd', age: 22},
+    //     { firstName: 'eee', lastName: 'eee', age: 23}
+    // ]);
+    // const users = await AppDataSource.manager.find(User);
+    // console.log(users);
+
+    // const users = await AppDataSource.manager.findBy(User, {
+    //     age: 23
+    // });
+    // console.log(users);
+
+    // const [users, count] = await AppDataSource.manager.findAndCount(User);
+    // console.log(users, count);
+
+    // const [users, count] = await AppDataSource.manager.findAndCountBy(User, {
+    //     age: 23
+    // })
+    // console.log(users, count);
+
+    // const user = await AppDataSource.manager.findOne(User, {
+    //     select: {
+    //         firstName: true,
+    //         age: true
+    //     },
+    //     where: {
+    //         id: 60
+    //     },
+    //     order: {
+    //         age: 'ASC'
+    //     }
+    // });
+    // console.log(user);
+
+    // const users = await AppDataSource.manager.find(User, {
+    //     select: {
+    //         firstName: true,
+    //         age: true
+    //     },
+    //     where: {
+    //         id: In([60, 61, 64])
+    //     },
+    //     order: {
+    //         age: 'ASC'
+    //     }
+    // });
+    // console.log(users);
+
+    // try {
+    //     const user = await AppDataSource.manager.findOneOrFail(User, {
+    //         where: {
+    //             id: 666
+    //         }
+    //     });
+    //     console.log(user);
+    // }catch(e) {
+    //     console.log(e);
+    //     console.log('没找到该用户');
+    // }
+    
+    // const users = await AppDataSource.manager.query('select * from user where age in(?, ?)', [60, 61]);
+    // console.log(users);
+
+
+const queryBuilder = await AppDataSource.manager.createQueryBuilder();
+
+const user = await queryBuilder.select("user")
+    .from(User, "user")
+    .where("user.age = :age", { age: 21 })
+    .getOne();
+
+console.log(user);
+
+    // console.log("Inserting a new user into the database...")
+    // const user = new User()
+    // user.firstName = "Timber"
+    // user.lastName = "Saw"
+    // user.age = 25
+    // await AppDataSource.manager.save(user)
+    // console.log("Saved a new user with id: " + user.id)
+
+    // console.log("Loading users from the database...")
+    // const users = await AppDataSource.manager.find(User)
+    // console.log("Loaded users: ", users)
+
+    // console.log("Here you can setup and run express / fastify / any other framework.")
 
 }).catch(error => console.log(error))
