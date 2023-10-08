f74323df727ba3d21e8ebc829f956cd5bbb59dc9
thinkasany <480968828@qq.com>
2023-10-08 16:55:03 +0800

    feat(acl-test): add  @UseGuards(LoginGuard)

--- 55/acl-test/src/aaa/aaa.controller.ts
+++ 55/acl-test/src/aaa/aaa.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+import { LoginGuard } from 'src/login.guard';
 import { AaaService } from './aaa.service';
 import { CreateAaaDto } from './dto/create-aaa.dto';
 import { UpdateAaaDto } from './dto/update-aaa.dto';
@@ -8,26 +18,31 @@ export class AaaController {
   constructor(private readonly aaaService: AaaService) {}
 
   @Post()
+  @UseGuards(LoginGuard)
   create(@Body() createAaaDto: CreateAaaDto) {
     return this.aaaService.create(createAaaDto);
   }
 
   @Get()
+  @UseGuards(LoginGuard)
   findAll() {
     return this.aaaService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(LoginGuard)
   findOne(@Param('id') id: string) {
     return this.aaaService.findOne(+id);
   }
 
   @Patch(':id')
+  @UseGuards(LoginGuard)
   update(@Param('id') id: string, @Body() updateAaaDto: UpdateAaaDto) {
     return this.aaaService.update(+id, updateAaaDto);
   }
 
   @Delete(':id')
+  @UseGuards(LoginGuard)
   remove(@Param('id') id: string) {
     return this.aaaService.remove(+id);
   }
--- 55/acl-test/src/bbb/bbb.controller.ts
+++ 55/acl-test/src/bbb/bbb.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+import { LoginGuard } from 'src/login.guard';
 import { BbbService } from './bbb.service';
 import { CreateBbbDto } from './dto/create-bbb.dto';
 import { UpdateBbbDto } from './dto/update-bbb.dto';
@@ -8,26 +18,31 @@ export class BbbController {
   constructor(private readonly bbbService: BbbService) {}
 
   @Post()
+  @UseGuards(LoginGuard)
   create(@Body() createBbbDto: CreateBbbDto) {
     return this.bbbService.create(createBbbDto);
   }
 
   @Get()
+  @UseGuards(LoginGuard)
   findAll() {
     return this.bbbService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(LoginGuard)
   findOne(@Param('id') id: string) {
     return this.bbbService.findOne(+id);
   }
 
   @Patch(':id')
+  @UseGuards(LoginGuard)
   update(@Param('id') id: string, @Body() updateBbbDto: UpdateBbbDto) {
     return this.bbbService.update(+id, updateBbbDto);
   }
 
   @Delete(':id')
+  @UseGuards(LoginGuard)
   remove(@Param('id') id: string) {
     return this.bbbService.remove(+id);
   }
