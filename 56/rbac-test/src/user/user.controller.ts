f364868bde36d46ed2d091b8f7b4282d7c384851
thinkasany <480968828@qq.com>
2023-10-09 10:48:23 +0800

    feat(rbac-test): initData

--- 56/rbac-test/src/user/user.controller.ts
+++ 56/rbac-test/src/user/user.controller.ts
@@ -10,10 +10,25 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserLoginDto } from './dto/user-login.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('init')
+  async initData() {
+    await this.userService.initData();
+    return 'done';
+  }
+
+  @Post('login')
+  async login(@Body() loginUser: UserLoginDto) {
+    const user = await this.userService.login(loginUser);
+
+    console.log(user);
+    return 'success';
+  }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
