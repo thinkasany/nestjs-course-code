87127ca70d94a93f9072c910a0ffbde28b4d093e
thinkasany <480968828@qq.com>
2023-10-22 11:31:52 +0800

    feat(nest-multer-upload): initial commit

---
+++ 24/nest-multer-upload/src/app.controller.ts
@@ -0,0 +1,31 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('aaa')
+  @UseInterceptors(
+    FileInterceptor('aaa', {
+      dest: 'uploads',
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File, @Body() body) {
+    console.log('body', body);
+    console.log('file', file);
+  }
+}
