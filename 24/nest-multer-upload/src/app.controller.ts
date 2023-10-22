cd0fb09bb976863126e5b9b3f4b6f02e601d1020
thinkasany <480968828@qq.com>
2023-10-22 12:20:10 +0800

    feat(nest-multer-upload): 添加上传文件案例

--- 24/nest-multer-upload/src/app.controller.ts
+++ 24/nest-multer-upload/src/app.controller.ts
@@ -1,13 +1,25 @@
 import {
   Body,
   Controller,
+  FileTypeValidator,
   Get,
+  HttpException,
+  MaxFileSizeValidator,
+  ParseFilePipe,
   Post,
   UploadedFile,
+  UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  FileInterceptor,
+  FilesInterceptor,
+  FileFieldsInterceptor,
+  AnyFilesInterceptor,
+} from '@nestjs/platform-express';
 import { AppService } from './app.service';
+import { FileSizeValidationPipe } from './file-size-validation-pipe.pipe';
+import { MyFileValidator } from './MyFileValidator';
 
 @Controller()
 export class AppController {
@@ -25,6 +37,116 @@ export class AppController {
     }),
   )
   uploadFile(@UploadedFile() file: Express.Multer.File, @Body() body) {
+    console.log('body', body);
+    console.log('file', file);
+  }
+
+  // 多文件上传
+  @Post('bbb')
+  @UseInterceptors(
+    FilesInterceptor('bbb', 3, {
+      dest: 'uploads',
+    }),
+  )
+  uploadFiles(
+    @UploadedFiles() files: Array<Express.Multer.File>,
+    @Body() body,
+  ) {
+    console.log('body', body);
+    console.log('files', files);
+  }
+
+  // 有多个文件的字段
+  @Post('ccc')
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'aaa', maxCount: 2 },
+        { name: 'bbb', maxCount: 3 },
+      ],
+      {
+        dest: 'uploads',
+      },
+    ),
+  )
+  uploadFileFields(
+    @UploadedFiles()
+    files: { aaa?: Express.Multer.File[]; bbb?: Express.Multer.File[] },
+    @Body() body,
+  ) {
+    console.log('body', body);
+    console.log('files', files);
+  }
+
+  @Post('ddd')
+  @UseInterceptors(
+    AnyFilesInterceptor({
+      dest: 'uploads',
+    }),
+  )
+  uploadAnyFiles(
+    @UploadedFiles() files: Array<Express.Multer.File>,
+    @Body() body,
+  ) {
+    console.log('body', body);
+    console.log('files', files);
+  }
+
+  @Post('eee')
+  @UseInterceptors(
+    FileInterceptor('aaa', {
+      dest: 'uploads',
+    }),
+  )
+  uploadFile2(
+    @UploadedFile(FileSizeValidationPipe) file: Express.Multer.File,
+    @Body() body,
+  ) {
+    console.log('body', body);
+    console.log('file', file);
+  }
+
+  @Post('fff')
+  @UseInterceptors(
+    FileInterceptor('aaa', {
+      dest: 'uploads',
+    }),
+  )
+  uploadFile3(
+    @UploadedFile(
+      new ParseFilePipe({
+        exceptionFactory: (err) => {
+          throw new HttpException('xxx' + err, 404);
+        },
+        validators: [
+          new MyFileValidator({}),
+          new MaxFileSizeValidator({ maxSize: 1000 }),
+          new FileTypeValidator({ fileType: 'image/jpeg' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+    @Body() body,
+  ) {
+    console.log('body', body);
+    console.log('file', file);
+  }
+
+  @Post('hhh')
+  @UseInterceptors(
+    FileInterceptor('aaa', {
+      dest: 'uploads',
+    }),
+  )
+  uploadFile4(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [new MyFileValidator({})],
+      }),
+    )
+    file: Express.Multer.File,
+    @Body() body,
+  ) {
     console.log('body', body);
     console.log('file', file);
   }
