5fc900d1c1f7f9603bd2db4fa0b24094ad57358f
thinkasany <480968828@qq.com>
2023-10-24 01:31:12 +0800

    feat(custom-provider): 增加调试注解

--- 7/custom-provider/src/app.controller.ts
+++ 7/custom-provider/src/app.controller.ts
@@ -1,41 +1,120 @@
 import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// 通过 provide 指定注入的 token，通过 useClass 指定注入的对象的类，Nest 会自动对它做实例化再注入。
+// @Controller()
+// export class AppController {
+//   constructor(private readonly appService: AppService) {}
+//   @Get()
+//   getHello(): string {
+//     return this.appService.getHello();
+//   }
+// }
+
+// 如果不想用构造器注入，也可以属性注入：
+
+// @Controller()
+// export class AppController {
+//   @Inject(AppService)
+//   private readonly appService: AppService;
+//   @Get()
+//   getHello(): string {
+//     return this.appService.getHello();
+//   }
+// }
+
+// 如果 token 是字符串的话，注入的时候就要用 @Inject 手动指定注入对象的 token 了：
+// @Controller()
+// export class AppController {
+//   @Inject('app_service')
+//   private readonly appService: AppService;
+//   @Get()
+//   getHello(): string {
+//     return this.appService.getHello();
+//   }
+// }
+
+// 除了指定 class 外，还可以直接指定一个值，让 IOC 容器来注入。
+// @Controller()
+// export class AppController {
+//   @Inject(AppService)
+//   private readonly appService: AppService;
+
+//   @Inject('person')
+//   private readonly person: { name: string; age: number };
+//   @Get()
+//   getHello(): string {
+//     console.log(this.person);
+//     return this.appService.getHello();
+//   }
+// }
+
+// provider 的值可能是动态产生的，Nest 也同样支持：
+// @Controller()
+// export class AppController {
+//   @Inject(AppService)
+//   private readonly appService: AppService;
+//   @Inject('person')
+//   private readonly person: { name: string; age: number };
+
+//   @Inject('person3')
+//   private readonly person3: { name: string; desc: string };
+//   @Get()
+//   getHello(): string {
+//     console.log(this.person3);
+//     return this.appService.getHello();
+//   }
+// }
+
+// # useFactory 支持异步：
 @Controller()
 export class AppController {
   @Inject(AppService)
   private readonly appService: AppService;
-  @Inject('app_service')
-  private readonly appService2: AppService;
-  @Inject('person')
-  private readonly person: { name: string; age: number };
-  @Inject('person2')
-  private readonly person2: { name: string; desc: string };
-  @Inject('person3')
-  private readonly person3: { name: string; desc: string };
-  @Inject('person4')
-  private readonly person4: { name: string; age: number };
+
   @Inject('person5')
   private readonly person5: { name: string; desc: string };
-
-  // constructor(
-  //   private  readonly appService: AppService,
-  //   @Inject('app_service') private readonly appService2: AppService,
-  //   @Inject('person') private readonly person: {name: string, age: number},
-  //   @Inject('person2') private readonly person2: {name: string, desc: string},
-  //   @Inject('person3') private readonly person3: {name: string, desc: string},
-  //   @Inject('person4') private readonly person4: {name: string, age: number},
-  //   @Inject('person5') private readonly person5: {name: string, desc: string},
-  // ) {}
-
   @Get()
   getHello(): string {
-    console.log(this.appService2);
-    console.log(this.person);
-    console.log(this.person2);
-    console.log(this.person3);
-    console.log(this.person4);
     console.log(this.person5);
     return this.appService.getHello();
   }
 }
+// @Controller()
+// export class AppController {
+//   @Inject(AppService)
+//   private readonly appService: AppService;
+//   @Inject('app_service')
+//   private readonly appService2: AppService;
+//   @Inject('person')
+//   private readonly person: { name: string; age: number };
+//   @Inject('person2')
+//   private readonly person2: { name: string; desc: string };
+//   @Inject('person3')
+//   private readonly person3: { name: string; desc: string };
+//   @Inject('person4')
+//   private readonly person4: { name: string; age: number };
+//   @Inject('person5')
+//   private readonly person5: { name: string; desc: string };
+
+//   // constructor(
+//   //   private  readonly appService: AppService,
+//   //   @Inject('app_service') private readonly appService2: AppService,
+//   //   @Inject('person') private readonly person: {name: string, age: number},
+//   //   @Inject('person2') private readonly person2: {name: string, desc: string},
+//   //   @Inject('person3') private readonly person3: {name: string, desc: string},
+//   //   @Inject('person4') private readonly person4: {name: string, age: number},
+//   //   @Inject('person5') private readonly person5: {name: string, desc: string},
+//   // ) {}
+
+//   @Get()
+//   getHello(): string {
+//     console.log(this.appService2);
+//     console.log(this.person);
+//     console.log(this.person2);
+//     console.log(this.person3);
+//     console.log(this.person4);
+//     console.log(this.person5);
+//     return this.appService.getHello();
+//   }
+// }
--- 7/custom-provider/src/app.module.ts
+++ 7/custom-provider/src/app.module.ts
@@ -5,33 +5,68 @@ import { AppService } from './app.service';
 @Module({
   imports: [],
   controllers: [AppController],
+  // providers: [
+  //   {
+  //     provide: AppService,
+  //     useClass: AppService,
+  //   },
+  // ],
+  // providers: [
+  //   {
+  //     provide: 'app_service',
+  //     useClass: AppService,
+  //   },
+  // ],
+  // providers: [
+  //   AppService,
+  //   {
+  //     provide: 'person',
+  //     useValue: {
+  //       name: 'aaa',
+  //       age: 20,
+  //     },
+  //   },
+  // ],
+  // providers: [
+  //   AppService,
+  //   {
+  //     provide: 'person2',
+  //     useFactory() {
+  //       return {
+  //         name: 'bbb',
+  //         desc: 'cccc',
+  //       };
+  //     },
+  //   },
+  // ],
+  // providers: [
+  //   AppService,
+  //   {
+  //     provide: 'person',
+  //     useValue: {
+  //       name: 'aaa',
+  //       age: 20,
+  //     },
+  //   },
+  //   {
+  //     provide: 'person3',
+  //     useFactory(person: { name: string }, appService: AppService) {
+  //       return {
+  //         name: person.name,
+  //         desc: appService.getHello(),
+  //       };
+  //     },
+  //     inject: ['person', AppService],
+  //   },
+  // ],
+  // # useFactory 支持异步：
   providers: [
     AppService,
-    {
-      provide: 'app_service',
-      useClass: AppService,
-    },
-    {
-      provide: 'person',
-      useValue: {
-        name: 'aaa',
-        age: 20,
-      },
-    },
-    {
-      provide: 'person2',
-      useFactory() {
-        return {
-          name: 'bbb',
-          desc: 'cccc',
-        };
-      },
-    },
     {
       provide: 'person5',
       async useFactory() {
         await new Promise((resolve) => {
-          setTimeout(resolve, 3000);
+          setTimeout(resolve, 5000);
         });
         return {
           name: 'bbb',
@@ -39,20 +74,55 @@ import { AppService } from './app.service';
         };
       },
     },
-    {
-      provide: 'person3',
-      useFactory(person: { name: string }, appService: AppService) {
-        return {
-          name: person.name,
-          desc: appService.getHello(),
-        };
-      },
-      inject: ['person', AppService],
-    },
-    {
-      provide: 'person4',
-      useExisting: 'person2',
-    },
   ],
+  // providers: [
+  //   AppService,
+  //   {
+  //     provide: 'app_service',
+  //     useClass: AppService,
+  //   },
+  //   {
+  //     provide: 'person',
+  //     useValue: {
+  //       name: 'aaa',
+  //       age: 20,
+  //     },
+  //   },
+  //   {
+  //     provide: 'person2',
+  //     useFactory() {
+  //       return {
+  //         name: 'bbb',
+  //         desc: 'cccc',
+  //       };
+  //     },
+  //   },
+  //   {
+  //     provide: 'person5',
+  //     async useFactory() {
+  //       await new Promise((resolve) => {
+  //         setTimeout(resolve, 3000);
+  //       });
+  //       return {
+  //         name: 'bbb',
+  //         desc: 'cccc',
+  //       };
+  //     },
+  //   },
+  //   {
+  //     provide: 'person3',
+  //     useFactory(person: { name: string }, appService: AppService) {
+  //       return {
+  //         name: person.name,
+  //         desc: appService.getHello(),
+  //       };
+  //     },
+  //     inject: ['person', AppService],
+  //   },
+    // {
+    //   provide: 'person4',
+    //   useExisting: 'person2',
+    // },
+  // ],
 })
-export class AppModule {}
+export class AppModule { }
